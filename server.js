31a6a1bd71ba6dfa4604bbee98a47ba9e79e5da7
DaleSystemsPvtLtd <freelancer@dalesystems.com>
2017-04-30 16:33:03 +0530

    test heroku update

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const port = process.env.PORT || 3000;
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/')
+app.set('view engine', 'hbs');
 app.use( express.static(__dirname + '/public'));
+
+
 app.get("/", (req, res)=>{
-    res.send("app works");
+    res.render('home', {
+        pageTitle: 'Home Page'
+    });
 });
 
 app.listen(port, ()=>{
